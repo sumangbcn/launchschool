f736246a04e000d1775d90d89e73eee054dfa8f3
sumangbcn <suman@onlinecatering.es>
2017-03-10 12:19:14 +0100

    Added Variable excercises

---
+++ variables/flname.rb
@@ -0,0 +1,5 @@
+puts "What\`s your first name?"
+fname = gets.chomp
+puts "What\'s your last name?"
+lname = gets.chomp
+puts fname + " " + lname
---
+++ variables/name_mult.rb
@@ -0,0 +1,6 @@
+puts "What\'s your name?"
+name = gets.chomp
+
+10.times do
+  puts name
+end
